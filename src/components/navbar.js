c7af895103971415a3ffb86d0433d22c118dc55c
jinimbrancalhao <jinjinim717@gmail.comgit config --global user.email jinjinim717@gmail.com>
2022-12-01 15:22:34 -0500

    fix homepage link

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,34 +1,42 @@
 /* eslint-disable max-len */
-import {NavLink} from 'react-router-dom';
-import React, {useState} from 'react';
+/* eslint-disable*/
+import { NavLink } from 'react-router-dom'
+import React, { useState } from 'react'
 
 const Navbar = () => {
-  const [hamburgerState, setHamburgerState] = useState(false);
+  const [hamburgerState, setHamburgerState] = useState(false)
 
-  const openHamburger = function() {
+  const openHamburger = function () {
     if (hamburgerState === false) {
-      setHamburgerState(true);
+      setHamburgerState(true)
     } else {
-      setHamburgerState(false);
+      setHamburgerState(false)
     }
-  };
+  }
 
   return (
     <nav className="navbar-wrapper">
       <ul className="menu">
         <li className="logo-wrapper">
           <a className="logo">
-            <NavLink to= "/Dog-Adoption-App">
+            <NavLink to="/dog-adoption-frontend">
               <img alt="logo" src={require('../images/dog-logo.jpg')}></img>
             </NavLink>
           </a>
         </li>
         <li className="hamburger">
           <a className="list-icon">
-            <img onClick={openHamburger} alt="list-icon" src={require('../images/List.jpg')}></img>
-
+            <img
+              onClick={openHamburger}
+              alt="list-icon"
+              src={require('../images/List.jpg')}
+            ></img>
           </a>
-          <div className={`hamburger-list ${!hamburgerState ? 'hamburger-closed' : 'hamburger-opened'}`}>
+          <div
+            className={`hamburger-list ${
+              !hamburgerState ? 'hamburger-closed' : 'hamburger-opened'
+            }`}
+          >
             <ul>
               <li>
                 <a href="/alldogs">All Dogs</a>
@@ -53,50 +61,42 @@ const Navbar = () => {
       <ul className="dropdown">
         <li className="search-icon">
           <a>
-            <NavLink to= "">
-              <img alt="search-icon" src={require('../images/MagnifyingGlass.jpg')}></img>
+            <NavLink to="">
+              <img
+                alt="search-icon"
+                src={require('../images/MagnifyingGlass.jpg')}
+              ></img>
             </NavLink>
           </a>
         </li>
         <li className="nav-link">
           <a>
-            <NavLink to= "/alldogs">
-              All dogs
-            </NavLink>
+            <NavLink to="/alldogs">All dogs</NavLink>
           </a>
         </li>
         <li className="nav-link">
           <a>
-            <NavLink to= "/retrievers">
-              Retriever
-            </NavLink>
+            <NavLink to="/retrievers">Retriever</NavLink>
           </a>
         </li>
         <li className="nav-link">
           <a>
-            <NavLink to= "/bulldogs">
-            Bulldog
-            </NavLink>
+            <NavLink to="/bulldogs">Bulldog</NavLink>
           </a>
         </li>
         <li className="nav-link">
           <a>
-            <NavLink to= "/corgis">
-            Corgi
-            </NavLink>
+            <NavLink to="/corgis">Corgi</NavLink>
           </a>
         </li>
         <li className="nav-link">
           <a>
-            <NavLink to= "/samoyeds">
-            Samoyed
-            </NavLink>
+            <NavLink to="/samoyeds">Samoyed</NavLink>
           </a>
         </li>
       </ul>
-
     </nav>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
